9d5c2b02c5955344c29321296c62bf51f18186b2
Roby Morgan <roby.c.morgan@gmail.com>
2021-10-30 00:53:45 +0200

    Album page done.

--- Homepage/album.js
+++ Homepage/album.js
@@ -4,7 +4,7 @@ window.onload = () => {
 
 const query = new URLSearchParams(document.location.search).get("albumId")
 console.log(query)
-let tracks = []
+
 
 const fetchAlbum = () => {
     fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${query}`, 
@@ -14,21 +14,45 @@ const fetchAlbum = () => {
     .then((response) => response.json())
     .then(data => {
         console.log(data)
-        tracks = data.tracks
-    })
-    .then(dislplayTracks)
-    .catch((err) => console.log(err))
-}
-const dislplayTracks = () => {
-    const tracksContainer = document.querySelector('.tracks-container')
-    tracksContainer.innerHTML = tracks.data.map((element) => {
+        const coverParent = document.querySelector('.album-parent')
+        const album =
         `
-        <div class="col-5">
-            <div class="mb-3 ml-3 pb-4">1 <span class="ml-4"><strong>${element.title}</strong><br><span class="singer-name">${element.artist.name}</span></span></div>
-            <div class="col-5"></div>
+        <div class="col-lg-2 col-sm-6 mt-2">
+            <img src="${data.cover_big}" height="180px" alt="">
+          </div>
+          <div class="col-lg-10 col-sm-6 align-self-end">
+            <div>
+                <small>${data.type}</small>
+            </div>
+            <div>
+               <h2>${data.artist.name}</h2>
+            </div>
+            <div class="">
+              <p> <small> <strong>
+              <img src="${data.cover_small}" height="22px" alt="" style="border-radius: 2rem;">
+              <a href="./Artistpage/artist.html">${data.artist.name}</a>. 
+              </strong>2018<strong> . </strong>${data.tracks.data.length}songs<strong>, </strong> 1hr 19 min</small></p>
+            </div>
+          </div>
+        
+        ` 
+        coverParent.innerHTML = album
+        tracksParent = document.querySelector('.tracks-container')
+        tracksParent.innerHTML = data.tracks.data.map(track => 
+            `
+            <div class="col-5">
+                <div class="mb-3 ml-3 pb-4">1 <span class="ml-4"><strong>${track.title}</strong><br><span
+                class="singer-name">$${track.artist.name}</span></span></div>
+            </div>
+            <div class="col-5">
+
+            </div>
             <div class="col-2">
-            <div class="mb-3 ml-3 pb-5">${element.duration}</div>
-        </div>
-        `
+                <div class="mb-3 ml-3 pb-5">${track.duration}</div>
+            </div>
+
+            `
+            ).join('')
     })
+    .catch((err) => console.log(err))
 }
