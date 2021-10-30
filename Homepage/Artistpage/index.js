614a7bbc1cb23bf61de87e3590cf100c1c087cb3
Roby Morgan <roby.c.morgan@gmail.com>
2021-10-30 02:31:04 +0200

    Created h1 dinamically.

--- Homepage/Artistpage/index.js
+++ Homepage/Artistpage/index.js
@@ -1,13 +1,31 @@
-window.onload = () => {
-    fetchArtist()
+
+const displayData = (data) => {
+    const artistName = document.getElementById('header-head') 
+    artistName.innerText = data.name
+}
+const fetchAlbums = (query) => {
+    fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${query}`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+    })
+    .catch((err) => console.log(err))
+
 }
 
+window.onload = () => {
+    fetchArtist()
+    
+}
 const query = new URLSearchParams(document.location.search).get("artistId")
-console.log
 const fetchArtist = () => {
     fetch(`https://striveschool-api.herokuapp.com/api/deezer/artist/${query}`)
     .then((response) => response.json())
     .then((data) => {
         console.log(data)
+        displayData(data)
+        fetchAlbums(data.name)
     })
+    .catch((err) => console.log(err))
+
 }
