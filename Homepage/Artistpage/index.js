77dae122e423cde068f3a572799a0c703420c10a
Roby Morgan <roby.c.morgan@gmail.com>
2021-10-30 23:14:01 +0200

    could not display album cards in the artist page.

--- Homepage/Artistpage/index.js
+++ Homepage/Artistpage/index.js
@@ -1,21 +1,8 @@
 
-const displayData = (data) => {
-    const artistName = document.getElementById('header-head') 
-    artistName.innerText = data.name
-}
-const fetchAlbums = (query) => {
-    fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${query}`)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data)
-    })
-    .catch((err) => console.log(err))
 
-}
 
 window.onload = () => {
     fetchArtist()
-    
 }
 const query = new URLSearchParams(document.location.search).get("artistId")
 const fetchArtist = () => {
@@ -25,6 +12,33 @@ const fetchArtist = () => {
         console.log(data)
         displayData(data)
         fetchAlbums(data.name)
+    })
+    .catch((err) => console.log(err))
+
+}
+const displayData = (data) => {
+    const artistName = document.getElementById('header-head') 
+    artistName.innerText = data.name
+}
+const fetchAlbums = (query) => {
+    fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${query}`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        console.log(data.data)
+        const albumContainer = document.querySelector('.albums-container')
+        albumContainer.innerHTML = data.data.map(element => {
+            `
+            <div class="card img-fluid" style="width: 18rem; background-image: url(${element.album.cover_small}); object-fit: cover;">
+                <div class="card-body">
+                <h5 class="card-title">${element.title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p class="card-text">${element.artist.name}</p>
+                </div>
+            </div>
+
+            `
+        }).join('')
     })
     .catch((err) => console.log(err))
 
